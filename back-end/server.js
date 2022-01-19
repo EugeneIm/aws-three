badb4ab031c4aa8ee33c30cc1cb9ad36e964a964
Your Name <you@example.com>
2022-01-19 09:04:07 -0800

    commit

---
+++ back-end/server.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const fs = require('fs')
+const multer = require('multer')
+
+const app = express()
+
+app.listen(8080, () => console.log("listening on port 8080"))
---
+++ front-end/frontend/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Here is the Front end</h1>
+    </div>
+  );
+}
+
+export default App;
